f4cf9036237af1901cd70d53bc511f70a76a2b57
David Dumas <david@dumas.io>
2019-11-17 23:02:47 -0500

    Description/about modal popup and better dataset chooser

--- slview/app.js
+++ slview/app.js
@@ -3,26 +3,68 @@ import * as THREE from './lib/three.js/build/three.module.js';
 import Stats from './lib/three.js/examples/jsm/libs/stats.module.js';
 import { TrackballControls } from './lib/three.js/examples/jsm/controls/TrackballControls.js';
 
-const default_dataset = 'psl2z';
+const default_dataset = 'PSL(2,Z) [120k]';
 
 const datasets = {
-    psl2z: 'data/psl2z-medium.json',
-    psl2zbig: 'data/psl2z-big.json',
-    psl2ztiny: 'data/psl2z-tiny.json',
-    disc6: 'data/disc6.json',
-    disc14: 'data/disc14.json',
-    ex_hyp1: 'data/ex-hyp1.json',
-    ex_hyp2: 'data/ex-hyp2.json',
-    ex_hypmany: 'data/ex-hypmany.json',
-    ex_parabolics2: 'data/ex-parabolics2.json',
-    ex_elliptics1:  'data/ex-elliptics1.json',
-    ex_elliptics2:  'data/ex-elliptics2.json',
-    ex_ellipticsmany:  'data/ex-ellipticsmany.json',
-    ex_halfturns: 'data/ex-halfturns.json',
-    ex_alltypes: 'data/ex-alltypes.json',
-    ex_test: 'data/ex-test.json',
+    'PSL(2,Z) [120k]':
+    { url: 'data/psl2z-medium.json',
+      longdesc: 'The projectivized integer unimodular group.  Approximately 120,000 elements.',
+    },
+    'PSL(2,Z) [750k]':
+    { url: 'data/psl2z-big.json',
+      longdesc: 'The projectivized integer unimodular group.  Approximately 750,000 elements.',
+    },
+    'PSL(2,Z) [12k]':
+    { url: 'data/psl2z-tiny.json',
+      longdesc: 'The projectivized integer unimodular group.  Approximately 12,000 elements.',
+    },
+    'Arith d=6 k=Q':
+    { url: 'data/disc6.json',
+      longdesc: 'The arithmetic Fuchsian group corresponding to a maximal order in the rational quaternion algebra of discriminant 6.  Geometrically, this is a (3,4,4) triangle group.  Approximately 370,000 elements.',
+    },
+    'Arith d=14 k=Q':
+    { url: 'data/disc14.json',
+      longdesc: 'The arithmetic Fuchsian group corresponding to a maximal order in the rational quaternion algebra of discriminant 14.  Approximately 125,000 elements.',
+    },
+    'Diag 1PSG':
+    { url: 'data/ex-hyp1.json',
+      longdesc: 'Elements of the diagonal subgroup of PSL(2,R) (hyperbolic elements fixing 0 and infinity.',
+    },
+    '2x Hyp 1PSG':
+    { url: 'data/ex-hyp2.json',
+      longdesc: 'Elements of two different hyperbolic 1-parameter subgroups of PSL(2,R), with fixed point pairs (0,infinity) and (-1,1).',
+    },
+    'Many Hyp 1PSG':
+    { url: 'data/ex-hypmany.json',
+      longdesc: 'Elements of several hyperbolic 1-parameter subgroups of PSL(2,R), all of whose axes pass through the imaginary unit.',
+    },
+    'Parabolics':
+    { url: 'data/ex-parabolics2.json',
+      longdesc: 'The upper- and lower-triangular parabolic subgroups of PSL(2,R).',
+    },
+    'Elliptic 1PSG':
+    { url: 'data/ex-elliptics1.json',
+      longdesc: 'Elliptic 1-parameter sugroup of PSL(2,R).',
+    },
+    '2x Elliptic 1PSG':
+    { url: 'data/ex-elliptics2.json',
+      longdesc: 'Two elliptic 1-parameter sugroups of PSL(2,R).',
+    },
+    'Many Elliptic 1PSG':
+    { url: 'data/ex-ellipticsmany.json',
+      longdesc: 'Many elliptic 1-parameter sugroups of PSL(2,R).',
+    },
+    'Halfturns':
+    { url: 'data/ex-halfturns.json',
+      longdesc: 'Many elliptic elements of order 2.' ,
+    },
+    'Trichotomy':
+    { url: 'data/ex-alltypes.json',
+      longdesc: 'Three one-parameter subgroups: Upper-triangular parabolics, elliptics fixing the imaginary unit, and diagonal hyperbolic elements.',
+    },
 }
 
+var showStats = false;
 var container, stats;
 var controls;
 var camera, cameraTarget, scene, renderer;
@@ -82,33 +124,52 @@ function init() {
 
     controls = new TrackballControls( camera, renderer.domElement );
     controls.rotateSpeed = 3.0;
-    
-    stats = new Stats();
-    container.appendChild( stats.dom );
 
+    if (showStats) {
+	stats = new Stats();
+	container.appendChild( stats.dom );
+    }
+    
     window.addEventListener( 'resize', onWindowResize, false );
     document.addEventListener( 'keydown', onKeyDown, false);
 
     window.onload = function() {
-	// GUI creation
-	var gui = new dat.GUI();
-	settings.particleSizeListener = gui.add(settings,'particleSize',0.00001,60);
-	settings.particleSizeListener.onChange(updateParticleSize);
-
-	settings.particleSizeListener = gui.add(settings,'particleAlpha',0.0,1.0).onChange(function(x) { particleMaterial.uniforms.alpha.value = x; })
-	
-	gui.add(settings,'dataset', Object.keys(datasets) ).onFinishChange(loadParticleCloud);
+	initStatus();
+	initGUI();
+        loadParticleCloud(settings.dataset);
     }
+}
 
-    loadParticleCloud(settings.dataset);
+function initGUI() {
+    var gui = new dat.GUI();
+    settings.particleSizeListener = gui.add(settings,'particleSize',0.00001,60);
+    settings.particleSizeListener.onChange(updateParticleSize);
+    
+    settings.particleSizeListener = gui.add(settings,'particleAlpha',0.0,1.0).onChange(function(x) { particleMaterial.uniforms.alpha.value = x; })
+
+    gui.add(settings,'dataset', Object.keys(datasets) ).onFinishChange(loadParticleCloud);
+}    
+
+
+function initStatus() {
+    window.addEventListener('click',function(event){
+	console.log(event.target);
+	if (event.target != document.getElementById("infobutton")) {
+	    document.getElementById('modal').style.display = 'none';
+	}
+    });
 }
 
 function loadParticleCloud(key) {
     var positions = [];
     var m22s = [];
+
+    document.getElementById('desc-short').innerHTML = 'Loading...';
+    document.getElementById('desc-title').innerHTML = '';
+    document.getElementById('desc-long').innerHTML = '';
     
     var request = new XMLHttpRequest();
-    request.open('GET', datasets[key], false);
+    request.open('GET', datasets[key]['url'], false);
     request.send(null);
     if (request.status === 200) {
 	var dataArr = JSON.parse(request.responseText);
@@ -129,7 +190,11 @@ function loadParticleCloud(key) {
 	scene.remove( particleCloud );
     
     particleCloud = new THREE.Points( geometry, particleMaterial );
-    scene.add( particleCloud );    
+    scene.add( particleCloud );
+
+    document.getElementById('desc-short').innerHTML = key;
+    document.getElementById('desc-title').innerHTML = key
+    document.getElementById('desc-long').innerHTML = datasets[key]['longdesc'];
 }
 
 function onKeyDown(event) {
@@ -146,6 +211,9 @@ function onKeyDown(event) {
             setParticleSize( settings.particleSize - 1 );
 	}
     }
+
+    // TODO: Check for ESC and close modal.
+    
     render();
 }
 
@@ -169,7 +237,9 @@ function onWindowResize() {
 function animate() {
     requestAnimationFrame( animate );    
     render();
-    stats.update();
+    if (showStats) {
+	stats.update();
+    }
     controls.update();
 }
 
